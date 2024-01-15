6e11974217e05c3740500394e832f36914a5fbf4
ishanrajpurohit-iitj <147649128+ishanrajpurohit-iitj@users.noreply.github.com>
2024-01-15 21:12:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Github from "./Github";
 import MyWork from "./MyWork"
 import './App.css';
 import AboutMe from "./AboutMe";
-
-ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root') // Adjust the element ID based on your HTML structure
-);
 
 const App = () => {
   return (
